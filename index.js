fc794de076ad133a02ab4ab9baa261fc8ea82646
Achilles Moraites <achimoraites@yahoo.gr>
2019-04-16 16:49:11 +0300

    chunks are of same size

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable require-jsdoc */
 const jimp = require('jimp');
 const fs = require('fs');
 
@@ -17,30 +18,72 @@ async function sliceImage(filename) {
   await slice2.writeAsync('slice2.jpg');
 }
 
-const imageChunks = async (filename, chunk, step = 0, chunks = []) => {
+async function imageChunks (filename, chunk, step = 0, chunks = [],move)  {
 
   const image = await jimp.read(filename);
   const { width, height } = image.bitmap;
 
-
-
   console.log(chunk, height);
   if(height <= (chunk + step)) {
-  //  let slice = image.clone().crop(0, step, width, step);
-    let slice = image.clone().autocrop().crop(0, step, width, (height - chunk));
-   // slice = await slice.autocrop(0);
-    const buffer = await slice.getBufferAsync(jimp.MIME_PNG);
+    const slice = image.clone().autocrop().crop(0, step, width, (height - step));
+    let buffer;
+    slice.getBuffer(jimp.MIME_PNG,(err, data)=> {
+      if(data) {
+        buffer = data;
+      }
+    });
     chunks.push(buffer);
     return chunks;
   } else {
-    const slice = image.clone().crop(0, step, width, chunk).autocrop();
-    const buffer = await slice.getBufferAsync(jimp.MIME_PNG);
+    let slice;
+    if(step == 0) {
+      slice = image.clone().crop(0, step, width, chunk).autocrop();
+    } else {
+      slice = image.clone().crop(0, step, width, chunk).autocrop();
+    }
+
+    let buffer;
+    slice.getBuffer(jimp.MIME_PNG,(err, data)=> {
+      if(data) {
+        buffer = data;
+      }
+    });
     chunks.push(buffer);
-    return imageChunks(filename, chunk + step, step + chunk, chunks);
+     return imageChunks(filename, chunk, step + chunk, chunks);
   }
 
+}
 
-};
+
+// const imageChunks = async (filename, chunk, step = 0, chunks = []) => {
+
+//   const image = await jimp.read(filename);
+//   const { width, height } = image.bitmap;
+
+
+
+//   console.log(chunk, height);
+//   if(height <= (chunk + step)) {
+//   //  let slice = image.clone().crop(0, step, width, step);
+//     const slice = image.clone().autocrop().crop(0, step, width, (height - chunk));
+//    // slice = await slice.autocrop(0);
+//     // const buffer = await slice.getBufferAsync(jimp.MIME_PNG);
+//     let buffer;
+//     slice.getBuffer(jimp.MIME_PNG,(err, data)=> {
+//       if(data) {
+//         buffer = data;
+//       }
+//     })
+//     chunks.push(buffer);
+//     return chunks;
+//   } else {
+//     const slice = image.clone().crop(0, step, width, chunk).autocrop();
+//     const buffer = await slice.getBufferAsync(jimp.MIME_PNG);
+//     chunks.push(buffer);
+//      return imageChunks(filename, chunk + step, step + chunk, chunks);
+//   }
+
+// };
 
 
 (async()=> {
@@ -50,9 +93,10 @@ try {
   const img = await jimp.read('2364.png');
   const {  height, width } = img.bitmap;
 
-  const split = 800;
- 
-  const chuncks = await imageChunks('2364.png',split,0);
+  const buffer = fs.readFileSync('2364.png');
+  const split = 200;
+  const chuncks = [];
+ await imageChunks(buffer,split,0, chuncks,split);
   console.log('Number of chunks', chuncks.length);
 
   console.log(chuncks);
